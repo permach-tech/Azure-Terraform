b07b88dd17877d8e3411c489ac28504af22e77ae
Persell Machuca <persell.machuca@gmail.com>
2024-04-03 00:13:33 -0500

    second commit

---
+++ Storage-Account/simple-storage-account/simple-storage.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.1"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-simple-sa"
+  location = "southcentralus"
+}
